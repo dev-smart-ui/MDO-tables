47001aa9ee895c324a8b5eb4e7faa18084d050c3
Shangri <shangrialena@gmail.com>
2024-09-18 00:04:37 +0200

    added types

---
+++ js_0525/tips-types.js
@@ -0,0 +1,125 @@
+const typesData=[
+    {
+        typeName:"undergroundOpenPit",
+        tipsText:"Underground & Open pit",
+        tipsLink:"#",
+    },
+    {
+        typeName:"undergroundPit",
+        tipsText:"Underground pit",
+        tipsLink:"#",
+    },
+    {
+        typeName:"openPit",
+        tipsText:"Open pit",
+        tipsLink:"#",
+    },
+    {
+        typeName:"cementedPasteBackfill",
+        tipsText:"Cemented paste backfill",
+        tipsLink:"#",
+    },
+    {
+        typeName:"cementedHydraulicFill",
+        tipsText:"Cemented hydraulic fill",
+        tipsLink:"#",
+    },
+    {
+        typeName:"cementedRockfill",
+        tipsText:"Cemented rockfill",
+        tipsLink:"#",
+    },
+    {
+        typeName:"cementedBackfillUndefined",
+        tipsText:"Cemented backfill (undefined)",
+        tipsLink:"#",
+    },
+    {
+        typeName:"unconsolidatedRockfill",
+        tipsText:"Unconsolidated rockfill",
+        tipsLink:"#",
+    },
+    {
+        typeName:"undefinedBackfill",
+        tipsText:"Undefined backfill",
+        tipsLink:"#",
+    },
+    {
+        typeName:"indirectOwner",
+        tipsText:"Indirect owner </br>(e.g. through a subsidiary, a JV, etc.)",
+        tipsLink:"#",
+    },
+    {
+        typeName:"direct",
+        tipsText:"Direct",
+        tipsLink:"#",
+    },
+    {
+        typeName:"calculatedByAi",
+        tipsText:"Calculated by AI",
+        tipsLink:"#",
+    },
+    {
+        typeName:"forecast",
+        tipsText:"Forecast",
+        tipsLink:"#",
+    },
+    {
+        typeName:"netOfByProduct",
+        tipsText:"Net of By-product",
+        tipsLink:"#",
+    },
+    {
+        typeName:"forecastCombinedProduction",
+        tipsText:"Forecast/Combined Production",
+        tipsLink:"#",
+    },
+    {
+        typeName:"electricVehicle",
+        tipsText:"Electric Vehicle",
+        tipsLink:"#",
+    },
+    {
+        typeName:"automaticVehicle",
+        tipsText:"Automatic Vehicle",
+        tipsLink:"#",
+    },
+    {
+        typeName:"required",
+        tipsText:"Required",
+        tipsLink:"#",
+    },
+    {
+        typeName:"equivalent",
+        tipsText:"Equivalent",
+        tipsLink:"#",
+    },
+    {
+        typeName:"production",
+        tipsText:"Production",
+        tipsLink:"#",
+    },
+    {
+        typeName:"depthUnknown",
+        tipsText:"Depth unknown",
+        tipsLink:"#",
+    },
+    {
+        typeName:"Reserves unknown",
+        tipsText:"reservesUnknown",
+        tipsLink:"#",
+    },
+    {
+        typeName:"reservesUnknown2",
+        tipsText:"Reserves unknown",
+        tipsLink:"#",
+    },
+    {
+        typeName:"reservesUnknown3",
+        tipsText:"Reserves unknown",
+        tipsLink:"#",
+    },
+
+
+
+]
