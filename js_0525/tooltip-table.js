8ae5097c83e6186a6f1fd6f26f50d29255a0d513
Shangri <shangrialena@gmail.com>
2024-09-20 14:51:39 +0200

    added pips on desktop

--- js_0525/tooltip-table.js
+++ js_0525/tooltip-table.js
@@ -4,32 +4,32 @@ let eventData;
 
 // Function to update isDesktop based on window width
 function updateIsDesktop() {
-  isDesktop = window.innerWidth > 1150;
-  document.dispatchEvent(tooltipTableEvent); // Trigger the tooltipTableEvent when isDesktop changes
+    isDesktop = window.innerWidth > 1150;
+    document.dispatchEvent(tooltipTableEvent); // Trigger the tooltipTableEvent when isDesktop changes
 
-  document.querySelectorAll(".custom-scroll").forEach((container) => {
-    if (container.classList.contains("search-results-container")) {
-      const theadHeight = window.innerWidth > 1919 ? 54 : 82;
-      $(".search-results-container.custom-scroll .ps__rail-y").css(
-        "margin",
-        `${theadHeight}px 0`,
-      );
-    }
-  });
+    document.querySelectorAll(".custom-scroll").forEach((container) => {
+        if (container.classList.contains("search-results-container")) {
+            const theadHeight = window.innerWidth > 1919 ? 54 : 82;
+            $(".search-results-container.custom-scroll .ps__rail-y").css(
+                "margin",
+                `${theadHeight}px 0`,
+            );
+        }
+    });
 }
 
 // Event listener for window resize
 window.addEventListener("resize", updateIsDesktop);
 
 function updateTopPositionForSelector() {
-  if (!isDesktop && $(".select-commodity").length) {
-    const selectorPositionOnPage = $(".select-commodity").offset().top;
-    const selectorHeight = $(".select-commodity").height();
-    $(".ui-selectmenu-menu").css(
-      "top",
-      `${selectorPositionOnPage + selectorHeight}px`,
-    );
-  }
+    if (!isDesktop && $(".select-commodity").length) {
+        const selectorPositionOnPage = $(".select-commodity").offset().top;
+        const selectorHeight = $(".select-commodity").height();
+        $(".ui-selectmenu-menu").css(
+            "top",
+            `${selectorPositionOnPage + selectorHeight}px`,
+        );
+    }
 }
 
 window.addEventListener("scroll", updateTopPositionForSelector);
@@ -41,115 +41,128 @@ var assetTooltips = {};
 var infoTooltips = {};
 
 $(window).on('resize load', debounce(() => {
-  var mobileTooltips = $(".mobile-tooltip");
+    var mobileTooltips = $(".mobile-tooltip");
 
-  $(mobileTooltips).each((i, element) => {
-    var parentTd = $(element).parents("td");
-    var aLink = $(element).parent().find("a");
-    var aLinkText = aLink.text();
-    var assetSpanId = aLink.parent().attr("id");
-    var flag = $(element).parents(".asset-label").find(".flag");
-    var isCurrentElementFlagIcon = $(element).hasClass("flag");
+    $(mobileTooltips).each((i, element) => {
+        var parentTd = $(element).parents("td");
+        var aLink = $(element).parent().find("a");
+        var aLinkText = aLink.text();
+        var assetSpanId = aLink.parent().attr("id");
+        var flag = $(element).parents(".asset-label").find(".flag");
+        var isCurrentElementFlagIcon = $(element).hasClass("flag");
 
-    var tooltipAdditionalText = flag.hasClass("multi-operation")
-                                ? "Multi - mine Operation"
-                                : flag.hasClass("single-operation")
-                                  ? "Part of an Operation"
-                                  : "";
+        var tooltipAdditionalText = flag.hasClass("multi-operation")
+            ? "Multi - mine Operation"
+            : flag.hasClass("single-operation")
+                ? "Part of an Operation"
+                : "";
 
-    var fullTooltipText =
-      `<a href="${aLink.attr('href')}" target="_blank" style="color: var(--color-blue);" class="tippy-tooltip-text-a">
+        var fullTooltipText =
+            `<a href="${aLink.attr('href')}" target="_blank" style="color: var(--color-blue);" class="tippy-tooltip-text-a">
             ${aLinkText.trim()} <br> ${tooltipAdditionalText.trim()}
       </a>
       <span class="tippy-tooltip-text-span">${aLinkText.trim()} <br> ${tooltipAdditionalText.trim()}</span>`;
 
-    /**
-     * Do not show a tooltip on a flag icon.
-     */
-    if (
-      (!isCurrentElementFlagIcon &&
-        assetSpanId &&
-        tooltipAdditionalText.length) ||
-      (!isDesktop &&
-        !isCurrentElementFlagIcon &&
-        assetSpanId &&
-        element.offsetWidth < element.scrollWidth) ||
-      (!isDesktop &&
-        !isCurrentElementFlagIcon &&
-        assetSpanId &&
-        element.offsetWidth <= 31)
-    ) {
-      var tippyConfig = {
-        triggerTarget: parentTd[0],
-        content: fullTooltipText,
-        updateDuration: 450,
-        duration: 450,
-        delay: [500, 0],
-        allowHTML: true,
-        arrow: true,
-        interactive: true,
-        placement: "top",
-      };
+        /**
+         * Do not show a tooltip on a flag icon.
+         */
 
-      /**
-       * Check if tooltips instance was created and added to an object.
-       */
-      if (!assetTooltips[aLinkText]) {
-        var instance = tippy(element, tippyConfig);
-        assetTooltips[aLinkText] = instance;
-      }
-    } else {
-      if (assetTooltips[aLinkText]) {
-        assetTooltips[aLinkText].destroy();
-        delete assetTooltips[aLinkText];
-      }
-    }
+        if ((!isCurrentElementFlagIcon && assetSpanId && tooltipAdditionalText.length) || (!isDesktop && !isCurrentElementFlagIcon && assetSpanId && element.offsetWidth < element.scrollWidth) || (!isDesktop && !isCurrentElementFlagIcon && assetSpanId && element.offsetWidth <= 31)) {
+            var tippyConfig = {
+                triggerTarget: parentTd[0],
+                content: fullTooltipText,
+                updateDuration: 450,
+                duration: 450,
+                delay: [500, 0],
+                allowHTML: true,
+                arrow: true,
+                interactive: true,
+                placement: "top",
+            };
 
-    /**
-     * Add tooltips for Job title, Name, Ref, date.
-     */
-    var key = $(element).text().replace(/[^a-zA-Z0-9]/g, '') + i;
-    if (
-      $(element).hasClass("nowrap-text") &&
-      !isDesktop &&
-      element.offsetWidth < element.scrollWidth
-    ) {
-      var tippyConfig = {
-        content: $(element).text(),
-        updateDuration: 450,
-        duration: 450,
-        delay: [500, 0],
-        arrow: true,
-        interactive: true,
-        placement: "top",
-      };
+            /**
+             * Check if tooltips instance was created and added to an object.
+             */
+            if (!assetTooltips[aLinkText]) {
+                var instance = tippy(element, tippyConfig);
+                assetTooltips[aLinkText] = instance;
+            }
+        } else {
+            if (assetTooltips[aLinkText]) {
+                assetTooltips[aLinkText].destroy();
+                delete assetTooltips[aLinkText];
+            }
+        }
 
-      /**
-       * Check if tooltips instance was created and added to an object.
-       */
-      if (!infoTooltips[key]) {
-        var instance = tippy(element, tippyConfig);
-        infoTooltips[key] = instance;
-      }
-    } else {
-      if (infoTooltips[key]) {
-        infoTooltips[key].destroy();
-        delete infoTooltips[key];
-      }
-    }
-  });
+        //show type when a row does not fit in a cell on the desktop
+        const maximumNumberOfCharacters=43
+        var isShowTipsOnDesktop = aLink.text().length >= maximumNumberOfCharacters;
+
+        if (isShowTipsOnDesktop && isDesktop) {
+            var tippyConfig = {
+                triggerTarget: parentTd[0],
+                content: $(element).text(),
+                updateDuration: 450,
+                duration: 450,
+                delay: [500, 0],
+                arrow: true,
+                interactive: true,
+                placement: "top",
+            };
+
+            if (!assetTooltips[aLinkText]) {
+                var instance = tippy(element, tippyConfig);
+                assetTooltips[aLinkText] = instance;
+            }
+        }
+
+
+        /**
+         * Add tooltips for Job title, Name, Ref, date.
+         */
+        var key = $(element).text().replace(/[^a-zA-Z0-9]/g, '') + i;
+        if ($(element).hasClass("nowrap-text") &&
+            !isDesktop &&
+            element.offsetWidth < element.scrollWidth
+        ) {
+            var tippyConfig = {
+                content: $(element).text(),
+                updateDuration: 450,
+                duration: 450,
+                delay: [500, 0],
+                arrow: true,
+                interactive: true,
+                placement: "top",
+            };
+
+
+            /**
+             * Check if tooltips instance was created and added to an object.
+             */
+            if (!infoTooltips[key]) {
+                var instance = tippy(element, tippyConfig);
+                infoTooltips[key] = instance;
+            }
+        } else {
+            if (infoTooltips[key]) {
+                infoTooltips[key].destroy();
+                delete infoTooltips[key];
+            }
+        }
+    });
+
 }, 300));
 
 /**
  * Hide tooltips on scroll inside table
  */
 $('#UpdatePanel').ready(function () {
-  $('.search-results-container.custom-scroll').on('ps-scroll-x ps-scroll-y', () => {
-    Object.values(assetTooltips).forEach((element) => {
-      element.hide();
+    $('.search-results-container.custom-scroll').on('ps-scroll-x ps-scroll-y', () => {
+        Object.values(assetTooltips).forEach((element) => {
+            element.hide();
+        });
+        Object.values(infoTooltips).forEach((element) => {
+            element.hide();
+        });
     });
-    Object.values(infoTooltips).forEach((element) => {
-      element.hide();
-    });
-  });
 });
